c63f5e42afad151b00548432ca821450f723949b
Lars Magnus Nordeide <nordeide@gmail.com>
2014-06-14 14:15:40 +0200

    First real test for Tournament class
    Renamed DataParser to Tournament and wrote the first real test

---
+++ webstats/SubmittedData.Test/TournamentTest.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Lars Magnus
+ * Date: 12.06.2014
+ * Time: 23:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using NUnit.Framework;
+using Shouldly;
+
+namespace SubmittedData.Test
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	[TestFixture]
+	public class TournamentTest
+	{
+		[Test]
+		public void TestRead_ShouldReadFile()
+		{
+			string wc2014 = @"name = ""VM 2014 Brasil""
+groups = [
+	[""Brasil"",		""Kroatia"", 		""Mexico"", 			""Kamerun""],
+	[""Spania"",		""Nederland"", 	""Chile"", 			""Australia""],
+	[""Colombia"",	""Hellas"", 		""Elfenbenskysten"", 	""Japan""],
+	[""Uruguay"",		""Costa Rica"",	""England"", 			""Italia""],
+	[""Sveits"",		""Ecuador"", 		""Frankrike"",		""Honduras""],
+	[""Argentina"",	""Bosnia"", 		""Iran"", 			""Nigeria""],
+	[""Tyskland"",	""Portugal"", 	""Ghana"", 			""USA""],
+	[""Belgia"",		""Algerie"", 		""Russland"",			""Soer-Korea""],
+]";
+			var fileSystem = new MockFileSystem( new Dictionary<string, MockFileData>
+			                                    {
+			                                    	{ @"data\vm2014.toml", new MockFileData(wc2014) }
+			                                    });
+				
+			var t = new Tournament(fileSystem);
+			string content = t.Read(@"data\vm2014.toml");
+			content.ShouldBe(wc2014);
+		}
+	}
+}
---
+++ webstats/SubmittedData/Tournament.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Lars Magnus
+ * Date: 12.06.2014
+ * Time: 23:46
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO.Abstractions;
+using System.Collections.Generic;
+
+namespace SubmittedData
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class Tournament
+	{
+		string _file;
+		readonly IFileSystem _fileSystem;
+
+		public Tournament(IFileSystem fileSystem)
+		{
+			_fileSystem = fileSystem;
+		}
+		
+		public Tournament() : this(
+			fileSystem: new FileSystem()
+		)
+		{
+		}
+			
+		public string Read(string file)
+		{
+			string text = _fileSystem.File.ReadAllText(file);
+			_file = file;
+			return text;
+		}
+		
+	}
+}
