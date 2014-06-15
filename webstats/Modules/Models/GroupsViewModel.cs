6d98b0b2bb331ca0d3e0e345cac22550ec9578bb
Lars Magnus Nordeide <nordeide@gmail.com>
2014-06-16 00:12:57 +0200

    Print groups to home page

--- webstats/Modules/Models/GroupsViewModel.cs
+++ webstats/Modules/Models/GroupsViewModel.cs
@@ -7,6 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections.Generic;
 using System.Text;
 using Nancy;
 using SubmittedData;
@@ -14,32 +15,67 @@ namespace Modules
 {
 	public class GroupsViewModel
 	{
-		public string Tournament {
-			get {
-				return _tournament.GetName();
-			}
+		ITournament _tournament;
+		
+		public string Tournament
+		{
+			get { return _tournament.GetName(); }
 		}
 
-		ITournament _tournament;
-
+		List<Group> _groups = new List<Group>();
+		public List<Group> Groups
+		{
+			get { return _groups; }
+			private set { _groups = value; }
+		}
+		
 		public GroupsViewModel(ITournament t)
 		{
 			_tournament = t;
+			CreateGroups();
 		}
 
-		private string PrintGroups()
+		private void CreateGroups()
 		{
-			StringBuilder s = new StringBuilder();
-			s.AppendFormat("Welcome to {0} betting scores\n", _tournament.GetName());
 			char gn = 'A';
-			foreach (object[] group in _tournament.GetGroups()) {
-				s.AppendLine("Group " + gn);
-				foreach (var team in group) {
-					s.AppendLine(team.ToString());
+			foreach (object[] group in _tournament.GetGroups())
+			{
+				var g = new Group() { Name = "Group " + gn };
+				foreach (var team in group)
+				{
+					g.Teams.Add(team.ToString());
 				}
+				_groups.Add(g);
 				gn++;
 			}
-			return s.ToString();
+		}
+		
+		public class Group
+		{
+			public string Name { get; set; }
+			
+			List<string> _teams = new List<string>();
+			public List<string> Teams
+			{
+				get { return _teams; }
+				private set { _teams = value; }
+			}
+			
+			public string TeamsAsHtml
+			{
+				get {
+					StringBuilder s = new StringBuilder();
+					s.Append("<ul>");
+					s.AppendLine();
+					foreach (var team in Teams)
+					{
+						s.AppendFormat("		<li>{0}</li>", team);
+						s.AppendLine();
+					}
+					s.Append("	</ul>");
+					return s.ToString();
+				}
+			}
 		}
 	}
 }
