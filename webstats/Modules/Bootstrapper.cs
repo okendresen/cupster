277d83b059c60becf21f8dfbaa662cf1c24eceed
Lars Magnus Nordeide <nordeide@gmail.com>
2014-06-20 23:23:28 +0200

    Print actual results on individual scoring card

--- webstats/Modules/Bootstrapper.cs
+++ webstats/Modules/Bootstrapper.cs
@@ -22,7 +22,7 @@ namespace Modules
 		{
 			// Register our app dependency as a normal singletons
 			var tournament = new Tournament();
-			tournament.Read(@"..\..\..\..\data\vm2014.toml");
+			tournament.Load(@"..\..\..\..\data\vm2014.toml");
 			container.Register<ITournament, Tournament>(tournament);
 			
 			var bets = new SubmittedBets();
@@ -30,6 +30,10 @@ namespace Modules
 			bets.ActualResultsFile = "vm2014-actual.toml";
 			bets.LoadAll(@"..\..\..\..\data");
 			container.Register<ISubmittedBets, SubmittedBets>(bets);
+
+			var results = new ActualResults();
+			results.Load(@"..\..\..\..\data\vm2014-actual.toml");
+			container.Register<IResults, ActualResults>(results);
 		}
 	}
 }
---
+++ webstats/SubmittedData.Test/ActualResultsTest.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Lars Magnus
+ * Date: 20.06.2014
+ * Time: 22:20
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+using NUnit.Framework;
+using Shouldly;
+
+namespace SubmittedData.Test
+{
+	/// <summary>
+	/// Description of ActualResults.
+	/// </summary>
+	[TestFixture]
+	public class ActualResultsTest
+	{
+		private ActualResults CreateTournament(string file, string content)
+		{
+			var fileSystem = new MockFileSystem( new Dictionary<string, MockFileData>
+			                                    {
+			                                    	{ file, new MockFileData(content) }
+			                                    });
+				
+			return new ActualResults(fileSystem);
+		}
+
+		[Test]
+		public void TestLoad_ShouldReadFile_IfExists()
+		{
+			string wc2014 = @"name = ""VM 2014 Brasil""";
+			var t = CreateTournament(@"data\vm2014-actual.toml", wc2014);
+			Should.NotThrow(() => t.Load(@"data\vm2014-actual.toml"));
+		}
+		
+		[Test]
+		public void TestLoad_ShouldFail_IfFileDoesNotExists()
+		{
+			string wc2014 = @"name = ""VM 2014 Brasil""";
+			var t = CreateTournament(@"data\vm2014-actual.toml", wc2014);
+			Should.Throw<FileNotFoundException>(() => t.Load(@"configs\vm2014-actual.toml"));
+		}
+		
+	}
+}
