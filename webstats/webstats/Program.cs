e83245d8f5cf989735e1a657485653412d4e2e00
Lars Magnus Nordeide <nordeide@gmail.com>
2014-07-02 21:01:56 +0200

    Make server able to run as deamon on mono

--- webstats/webstats/Program.cs
+++ webstats/webstats/Program.cs
@@ -7,24 +7,36 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Linq;
+using System.Threading;
 using Nancy.Hosting.Self;
 
 namespace webstats
 {
-	class Program
-	{
-		public static void Main(string[] args)
-		{
-			// Start web server
-			Console.WriteLine("Press enter to terminate server");
-			HostConfiguration hc = new HostConfiguration();
-			hc.UrlReservations.CreateAutomatically = true;
-			using (var host = new NancyHost(hc, new Uri("http://localhost:4444")))
-			{
-			   host.Start();
-			   Console.ReadLine();
-			}
-			Console.WriteLine("Server terminated");
-		}
-	}
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Start web server
+            Console.WriteLine("Press enter to terminate server");
+            HostConfiguration hc = new HostConfiguration();
+            hc.UrlReservations.CreateAutomatically = true;
+            var host = new NancyHost(hc, new Uri("http://localhost:8888"));
+            host.Start();
+            
+            //Under mono if you deamonize a process a Console.ReadLine with cause an EOF 
+            //so we need to block another way
+            if (args.Any(s => s.Equals("-d", StringComparison.CurrentCultureIgnoreCase)))
+            {
+                Thread.Sleep(Timeout.Infinite);
+            }
+            else
+            {
+                Console.ReadKey();
+            }
+
+            host.Stop();
+            Console.WriteLine("Server terminated");
+        }
+    }
 }
